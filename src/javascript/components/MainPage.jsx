2dd023a5a3254e0c31e2512a313694ebec6089a4
nitescumarisa <marisa.nitescu@gmail.com>
2022-05-07 17:10:48 +0300

    initial setup

---
+++ src/javascript/components/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainPage() {
+    return (
+        <div id="MainPage" className="text-3xl">
+            Salut
+        </div>
+    );
+}
+
+export default MainPage;
